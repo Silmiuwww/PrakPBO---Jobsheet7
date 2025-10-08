9d2fa2fa5939285811dea1cf6b0d38c6d7740797
Silmiuww <nadiasalma078@gmail.com>
2025-10-08 21:12:43 +0700

    Percobaan 6

--- Dosen.java
+++ Dosen.java
@@ -6,13 +6,17 @@ public class Dosen extends Pegawai {
         System.out.println("Objek dari class Dosen dibuat");
     }
 
-    public String getInfo() {
-        String info = "";
-        info += "NIP: " + super.nip + "\n";
-        info += "Nama: " + super.nama + "\n";
-        info += "Gaji: " + super.gaji + "\n";
-        info += "NIDN: " + this.nidn + "\n";
+    public Dosen(String nip, String nama, double gaji, String nidn) {
+        System.out.println("Objek dari class Dosen dibuat dengan constructor berparameter");
+    }
 
+    public String getInfo() {
+        return "NIDN: " + this.nidn + "\n";
+    }
+
+    public String getAllInfo() {
+        String info = super.getInfo();
+        info += this.getInfo();
         return info;
     }
 }
